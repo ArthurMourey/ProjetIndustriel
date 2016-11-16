03941977a070882f619769085627df1aeebfe3fe
ArthurMourey <mourey.arthur@gmail.com>
2016-11-16 15:50:37 +0100

    Navigation

---
+++ app/src/main/java/miage/org/projetindustriel/Login.java
@@ -0,0 +1,28 @@
+package miage.org.projetindustriel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Nom de l'application");
+        final Button loginButton = (Button) findViewById(R.id.connection_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Musique.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
