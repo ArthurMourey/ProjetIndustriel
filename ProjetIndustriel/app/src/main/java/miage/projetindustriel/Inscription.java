1db9a30ebb977bd3edf3197bc756a2d5ccf1b86e
Arthur Mourey <mourey.arthur@gmail.com>
2017-01-11 12:52:26 +0100

    Google Maps API + clean project

---
+++ ProjetIndustriel/app/src/main/java/miage/projetindustriel/Inscription.java
@@ -0,0 +1,83 @@
+package miage.projetindustriel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+
+        setTitle("Inscription");
+        final Button submit_inscription_button = (Button) findViewById(R.id.submit_inscription_button);
+        submit_inscription_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                List<String> keys = new ArrayList<String>();
+                List<String> values= new ArrayList<String>();
+                String login = "";
+                String mdp = "";
+                String prenom = "";
+                String nom = "";
+                String mail = "";
+
+                EditText loginEditText = (EditText) findViewById(R.id.login_inscription);
+                login += loginEditText.getText().toString();
+                EditText mdpEditText = (EditText) findViewById(R.id.mdp_inscription);
+                mdp += mdpEditText.getText().toString();
+                EditText prenomEditText = (EditText) findViewById(R.id.prenom_inscription);
+                prenom += prenomEditText.getText().toString();
+                EditText nomEditText = (EditText) findViewById(R.id.nom_inscription);
+                nom += nomEditText.getText().toString();
+                EditText mailEditText = (EditText) findViewById(R.id.mail_inscription);
+                mail += mailEditText.getText().toString();
+                keys.add("login_inscription");
+                keys.add("mdp_inscription");
+                keys.add("prenom_inscription");
+                keys.add("nom_inscription");
+                keys.add("mail_inscription");
+                values.add(login);
+                values.add(mdp);
+                values.add(prenom);
+                values.add(nom);
+                values.add(mail);
+
+                if((login=="")||(mdp=="")||(nom=="")||(prenom=="")||(mail=="")){
+                    TextView t = (TextView) findViewById(R.id.errorMessageInscription);
+                    t.setText("Veuillez remplir TOUS les champs ci-dessus.");
+                }
+                else{
+                    try {
+                        String response = DAO.post("http://www.madpumpkin.fr/index.php",keys, values);
+                        System.out.println(response);
+                        if(response.contains("SUCCESS")){
+                            Intent intent = new Intent(Inscription.this, Login.class);
+                            intent.putExtra("inscription",true);
+                            startActivity(intent);
+                        }
+                        else{
+                            TextView t = (TextView) findViewById(R.id.errorMessageInscription);
+                            t.setText("Le login que vous avez renseigné est déjà pris.");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+    }
+}
---
+++ ProjetIndustriel/app/src/main/java/miage/projetindustriel/Login.java
@@ -0,0 +1,111 @@
+package miage.projetindustriel;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Nom de l'application");
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        //Si une inscription s'est correctement effectuée
+        Boolean inscription;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                inscription = null;
+            } else {
+                inscription = extras.getBoolean("inscription");
+                TextView t = (TextView) findViewById(R.id.errorMessage);
+                t.setTextColor(Color.GREEN);
+                t.setText("Inscription réussie.");
+            }
+        } else {
+            inscription= (Boolean) savedInstanceState.getSerializable("inscription");
+            TextView t = (TextView) findViewById(R.id.errorMessage);
+            t.setTextColor(Color.GREEN);
+            t.setText("Inscription réussie.");
+        }
+
+        final Button loginButton = (Button) findViewById(R.id.connection_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                List<String> keys = new ArrayList<String>();
+                List<String> values= new ArrayList<String>();
+                EditText loginEditText = (EditText) findViewById(R.id.login);
+                String login = loginEditText.getText().toString();
+                EditText mdpEditText = (EditText) findViewById(R.id.password);
+                String mdp = mdpEditText.getText().toString();
+                keys.add("login");
+                keys.add("password");
+                values.add(login);
+                values.add(mdp);
+                boolean isValid = false;
+                try {
+                    String response = DAO.post("http://www.madpumpkin.fr/index.php",keys, values);
+                    System.out.println(response);
+                    if(response.contains("SUCCESS")){
+                        isValid = true;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(isValid){
+                    Intent intent = new Intent(Login.this, Musique.class);
+                    startActivity(intent);
+                }
+                else {
+                    TextView t = (TextView) findViewById(R.id.errorMessage);
+                    t.setTextColor(Color.RED);
+                    t.setText("Login ou mot de passe invalide, veuillez réessayer.");
+                }
+            }
+        });
+
+        final Button inscriptionButton = (Button) findViewById(R.id.inscription_button);
+        inscriptionButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Inscription.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+
