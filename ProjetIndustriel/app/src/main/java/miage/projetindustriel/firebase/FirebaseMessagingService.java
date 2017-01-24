8f057a978b17f0f1cb765fbac272bece353a89f4
Arthur Mourey <mourey.arthur@gmail.com>
2017-01-24 23:17:20 +0100

    Notifications

--- ProjetIndustriel/app/src/main/java/miage/projetindustriel/firebase/FirebaseMessagingService.java
+++ ProjetIndustriel/app/src/main/java/miage/projetindustriel/firebase/FirebaseMessagingService.java
@@ -2,11 +2,15 @@ package miage.projetindustriel.firebase;
 
 
 
-import android.content.Context;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
 
-import static android.content.ContentValues.TAG;
+import miage.projetindustriel.R;
+import miage.projetindustriel.controller.Login;
 
 /**
  * Created by Asus on 18/01/2017.
@@ -15,19 +19,25 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
-        // TODO(developer): Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-        //Log.d(TAG, "From: " + remoteMessage.getFrom());
-        //Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        System.out.println(remoteMessage.getData());
+        traitementMessage(remoteMessage.getData().get("message"));
     }
 
     private void traitementMessage(String message) {
-        System.out.println("TODO");
-        //TODO
+        Intent i = new Intent(this,Login.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle("Musy")
+                .setContentText(message)
+                .setSmallIcon(R.drawable.ic_play_dark)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(0,builder.build());
     }
 
 }
